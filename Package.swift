fa6e2cb1d904ef64621e298dd283d550c2cef599
Morten Bjerg Gregersen <morten@mogee.dk>
2019-05-08 21:40:09 +0200

    Removing SourceKitten

--- Package.swift
+++ Package.swift
@@ -10,14 +10,13 @@ let package = Package(
             targets: ["SwiftCodeCodable"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/jpsim/SourceKitten", from: "0.23.0"),
 		.package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0")),
 		.package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0"),
     ],
     targets: [
         .target(
             name: "SwiftCodeCodable",
-            dependencies: ["SourceKittenFramework", "SwiftSyntax"]),
+            dependencies: ["SwiftSyntax"]),
         .testTarget(
             name: "SwiftCodeCodableTests",
             dependencies: ["SwiftCodeCodable", "SwiftPM"]),
